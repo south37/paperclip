d778aedf0ad2c7444b7d0a701a5e2fc562d795b6
Nao Minami <south37777@gmail.com>
2017-05-17 18:04:30 +0900

    Use lambda for callback terminator

--- lib/paperclip/callbacks.rb
+++ lib/paperclip/callbacks.rb
@@ -7,7 +7,7 @@ module Paperclip
 
     module Defining
       def define_paperclip_callbacks(*callbacks)
-        define_callbacks *[callbacks, {:terminator => "result == false"}].flatten
+        define_callbacks *[callbacks, {:terminator => hasta_la_vista_baby}].flatten
         callbacks.each do |callback|
           eval <<-end_callbacks
             def before_#{callback}(*args, &blk)
@@ -17,6 +17,18 @@ module Paperclip
               set_callback(:#{callback}, :after, *args, &blk)
             end
           end_callbacks
+        end
+      end
+
+      private
+
+      def hasta_la_vista_baby
+        lambda do |_, result|
+          if result.respond_to?(:call)
+            result.call == false
+          else
+            result == false
+          end
         end
       end
     end
